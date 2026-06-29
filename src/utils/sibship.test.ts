9db6de6f234b6f5a1b02b372520bee16c832ad03
Joshua Ellis <joshua.drewitt.ellis@gmail.com>
2026-06-30 07:49:56 +0800

    refactor: annotate doc() test helper return type

--- src/utils/sibship.test.ts
+++ src/utils/sibship.test.ts
@@ -1,13 +1,13 @@
 import { describe, it, expect } from 'vitest';
 import { commonSibshipId } from './sibship';
 import { RelationshipType } from '../types/enums';
-import type { ParentChildRelationship } from '../types/pedigree';
+import type { ParentChildRelationship, PedigreeDocument } from '../types/pedigree';
 
 function link(id: string, childId: string, parentPartnershipId: string): ParentChildRelationship {
   return { id, type: RelationshipType.ParentChild, parentPartnershipId, childId, isAdoptive: false };
 }
 
-function doc(links: ParentChildRelationship[]) {
+function doc(links: ParentChildRelationship[]): Pick<PedigreeDocument, 'parentChildLinks'> {
   return { parentChildLinks: Object.fromEntries(links.map((l) => [l.id, l])) };
 }
 
